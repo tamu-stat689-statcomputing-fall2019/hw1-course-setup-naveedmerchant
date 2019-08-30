f84e18e0bec42144f99c1ff6455aecd60b11cb7b
Naveed Merchant <naveedmerchant2@gmail.com>
2019-08-30 10:22:35 -0500

    Used functions in LM.R to complete tasks and show that those functions are working properly.

--- SimpleProgram.R
+++ SimpleProgram.R
@@ -8,8 +8,16 @@ sigma = 2 # noise standard deviation
 beta = rep(2,p) # true vector of coefficients
 X = matrix(rnorm(n*p), n, p) # n by p matrix of predictors
 
+source("FunctionsLM.R")
+
 # [ToDo] Use generateY function to generate Y
+# Task now complete
+Y = generateY(X = X, beta = beta, sigma = sigma, seed = 50)
 
 # [ToDo] Use calculateBeta function to calculate beta_LS
+# Task now complete
 
+beta_LS = calculateBeta(X,Y)
 # [ToDo] Use calculateMSE to assess the estimation error measured by squared eucledian distance - ||beta - beta_LS||_2^2
+#Task now complete
+MSE = calculateMSE(beta = beta, beta_LS = beta_LS)
