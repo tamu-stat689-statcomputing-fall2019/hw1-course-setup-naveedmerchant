461e8590850c4a9b296c7cb2739318f6277d75c4
Naveed Merchant <naveedmerchant2@gmail.com>
2019-08-30 10:08:21 -0500

    Computed Forbenius norm by using crossprod(x) instead of sum(x)^2 for speed

--- FunctionsLM.R
+++ FunctionsLM.R
@@ -32,7 +32,7 @@ calculateBeta <- function(X, Y){
 
 # Calculate MSE
 calculateMSE <- function(beta, beta_LS){
-  MSE = sum((beta - beta_LS)^2)
+  MSE = as.numeric(crossprod(beta - beta_LS))
   # Return MSE - error ||beta - beta_LS||_2^2
   return(MSE)
 }
